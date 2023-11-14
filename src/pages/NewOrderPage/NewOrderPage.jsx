1d9540f6d673b24e25348ea74f0710c96ed59f7f
lvlivingston <lvlivingston@gmail.com>
2023-11-14 12:19:11 -0600

    added category string

--- src/pages/NewOrderPage/NewOrderPage.jsx
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -1,8 +1,10 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import * as itemsAPI from '../../utilities/items-api';
 
 export default function NewOrderPage() {
   const [menuItems, setMenuItems] = useState([]);
+  // Create and initialize the ref to an empty array
+  const categoriesRef = useRef([]);
 
   // - Fetch the menuItems from the server via AJAX
   // - When the data comes back, call setMenuItems to save in state
@@ -10,6 +12,8 @@ export default function NewOrderPage() {
   useEffect(function() {
     async function getItems() {
       const items = await itemsAPI.getAll();
+      // Remove dups of category names using a Set, then spread Set back into an array literal
+      categoriesRef.current = [...new Set(items.map(item => item.category.name))];
       setMenuItems(items);
     }
     getItems();
